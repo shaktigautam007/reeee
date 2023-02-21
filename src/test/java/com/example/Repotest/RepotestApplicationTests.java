02c00955434bf186ed6b2c8cbeae8059b82c37bc
shaktigautam007 <shaktigautam007@gmail.com>
2023-02-21 13:46:24 +0530

    Initial commit

---
+++ src/test/java/com/example/Repotest/RepotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Repotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
